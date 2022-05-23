b2dc6dea843645c7fb9d1f6d3152bdf0028fc989
spietras <szymon.pietras@magnet.coop>
2022-05-23 12:33:50 +0100

    Update clojars credentials for Github Actions

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/spec.alpha "0.3.214"]]
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
-                                      :username :env/clojars_username
-                                      :password :env/clojars_password
+                                      :username :env/CLOJARS_USERNAME
+                                      :password :env/CLOJARS_PASSWORD
                                       :sign-releases false}]
                         ["releases"  {:url "https://clojars.org/repo"
-                                      :username :env/clojars_username
-                                      :password :env/clojars_password
+                                      :username :env/CLOJARS_USERNAME
+                                      :password :env/CLOJARS_PASSWORD
                                       :sign-releases false}]]
   :profiles
   {:dev [:project/dev :profiles/dev]
